5c4f77afdcf1fc54e9016c20d316ab00bd8aa6e3
Diego Medina <dmedina@springbot.com>
2017-06-11 01:30:09 -0400

    Basic routing for rooms controller

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,15 @@
+class RoomsController < ApplicationController
+  def new
+    puts params
+  end
+
+  def create
+  end
+
+  def join
+    puts params
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resource :room, only: [:new, :create, :update] do
+    get :join
+  end
+
   get 'static/home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
